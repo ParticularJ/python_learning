f4dcd048710abdc1907a9414835eb0c0a9db6447
ParticularJ <344409038@qq.com>
2018-02-13 22:30:28 +0800

    modi

---
+++ string_ex.py
@@ -0,0 +1,13 @@
+my_name = 'ck'
+my_age = '12'
+my_eyes = 'Black'
+my_hair = 'Black'
+# the f means tell python print string format
+print(f"Let's talk about {my_name}")
+print(f"He's age is {my_age}")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+
+hilarious = False
+# .format() apply the already exist string
+joke_evaluation = "Isn't that joke so funny?! {}"
+print(joke_evaluation.format(hilarious))
